b28a841bb2ecade21546415126c6fcb99e9b1fe8
irigyano <110440243+irigyano@users.noreply.github.com>
2024-12-06 10:37:46 +0800

    day5 p1

---
+++ day5/part1.ts
@@ -0,0 +1,67 @@
+import path from "path";
+
+// const file = Bun.file(path.join(import.meta.dir, "example.txt"));
+const file = Bun.file(path.join(import.meta.dir, "input.txt"));
+
+const input = await file.text();
+
+function solution(input: string) {
+  let ans = 0;
+
+  const data = input.trim().split("\n");
+
+  const rules = [];
+  const updates = [];
+
+  let hasMetBlank = false;
+  for (const d of data) {
+    if (d === "") {
+      hasMetBlank = true;
+      continue;
+    }
+
+    if (!hasMetBlank) {
+      rules.push(d);
+    } else {
+      updates.push(d);
+    }
+  }
+
+  const rulesMap = new Map();
+  for (const rule of rules) {
+    const [target, index] = rule.split("|");
+
+    if (rulesMap.has(index)) {
+      rulesMap.get(index).add(target);
+    } else rulesMap.set(index, new Set([target]));
+  }
+
+  // check each update
+  for (const update of updates) {
+    const nums = update.split(",");
+
+    let isValid = true;
+    for (let i = 0; i < nums.length; i++) {
+      const num = nums[i];
+
+      if (rulesMap.has(num)) {
+        const set = rulesMap.get(num);
+
+        for (let j = i + 1; j < nums.length; j++) {
+          if (set.has(nums[j])) {
+            isValid = false;
+            break;
+          }
+        }
+      }
+    }
+
+    if (isValid) {
+      ans += parseInt(nums[Math.floor(nums.length / 2)]);
+    }
+  }
+
+  return ans;
+}
+
+console.log(solution(input));
